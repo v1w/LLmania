87753d7160f1a6b30935b4feb30bfc90e0ad51cb
v1w <v1w@users.noreply.github.com>
2016-09-17 12:18:46 +0800

    code rewrite

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 try:
-    import pyglet
-    from pyglet.gl import *
+
     import os
     import sys
     import json
     import time
+    import pyglet
+    from pyglet.gl import *
+    import matplotlib.pyplot as plt
     import libres
     import libgame
 
